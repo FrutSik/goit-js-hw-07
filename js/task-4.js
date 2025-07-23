fab2ec313a4ae81eba91181a1d8c2bca027dcc41
FrutSik <makapov172007@gmail.com>
2025-07-23 15:56:01 +0300

    task-3

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const { email, password } = this.elements;
+
+    if (!email.value.trim() || !password.value.trim()) {
+      alert("All form fields must be filled in");
+      return;
+    }
+
+    const formData = {
+      email: email.value.trim(),
+      password: password.value.trim(),
+    };
+
+    console.log(formData);
+
+    this.reset();
+  });
