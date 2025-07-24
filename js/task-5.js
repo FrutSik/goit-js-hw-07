4ef3d1f0100902914851821a45d02b4c1f8e5167
FrutSik <makapov172007@gmail.com>
2025-07-24 13:47:55 +0300

    end

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const changeColorBtn = document.querySelector(".change-color");
+  const changeColorSpan = document.querySelector(".color");
+
+  changeColorBtn.addEventListener("click", () => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    changeColorSpan.textContent = randomColor;
+  });
+});
